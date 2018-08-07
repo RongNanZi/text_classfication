17323a3a9d90cd925f8aa938a7d6f0bab266eb66
RongNanZi <467166703@qq.com>
2018-08-07 13:52:43 +0800

    grid search on linear SVM

--- SVModel.py
+++ SVModel.py
@@ -1,37 +1,49 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 import numpy as np
 from sklearn import metrics
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedShuffleSplit
 import pickle as p
-data_path = ""
-train = pd.read_csv(data_path+'train_set.csv')
-test = pd.read_csv(data_path+'test_set.csv')['article']
-
-vect = TfidfVectorizer(ngram_range=(1,3), max_df=0.9, min_df= 3, smooth_idf=1, sublinear_tf=1)
-train_num = vect.fit_transform(train['article'])
+import logging
+data_path = "./data/"
+train = pd.read_csv(data_path+'train_set.csv') 
+logging.basicConfig(filename='grid_search.log', filemode='w',level=logging.INFO)
+vect = TfidfVectorizer(ngram_range=(1,2), max_df=0.9, min_df= 3, smooth_idf=1, sublinear_tf=1)
+train_word_num = vect.fit_transform(train['article'])
+train_seg_num = vect.fit_transform(train['word_seg'])
+logging.info('the feature engine is over')
 train_label = np.asarray(train['class'], dtype=int)
-clf = SVC()
+clf = LinearSVC(dual=False)
 
 param_grid = [
-    {"kernel":["linear","rbf","sigmoid"]},
-    {'C':np.logspace(-2, 1, 10)},
-    {'gamma':np.logspace(-3, 2, 10)}
+        {'penalty':['l1','l2']},
+        {'C':[0.001,0.01,0.1,1]},
+        {'class_weight':['balanced']}
 ]
 cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
-grid = GridSearchCV(clf,param_grid=param_grid, cv=cv)
-grid.fit(train_num, train_label)
+word_grid = GridSearchCV(clf,param_grid=param_grid, cv=cv, n_jobs=8)
+word_grid.fit(train_word_num, train_label)
 
 #save the grid search result
-with open('./grid.result', 'wb') as f:
-    p.dump(grid, f)
+with open('./word_grid.result', 'wb') as f:
+    p.dump(word_grid, f)
+logging.info('the word-level linear svc grid search is over')
+seg_grid = GridSearchCV(clf,param_grid=param_grid, cv=cv, n_jobs=8)
+seg_grid.fit(train_seg_num, train_label)
 
-#predict the test csv
+#save the grid search result
+with open('./seg_grid.result', 'wb') as f:
+    p.dump(seg_grid,f)
+
+
+'''
+predict the test csv
 test_num = vect.transform(test['article'])
 e_svm = SVC(grid.best_params_)
 wanted = e_svm.predict(test_num)
 df_wanted = pd.DataFrame(wanted, columns=['class'])
 df_wanted['id'] = range(len(wanted))
 df_wanted[['id','class']].to_csv('./best_svm.csv', index=False)
+'''
--- test/SVModelTest.py
+++ test/SVModelTest.py
@@ -1,4 +1,3 @@
-print("code is b\n")
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
@@ -8,10 +7,14 @@ from sklearn.model_selection import StratifiedShuffleSplit
 import pickle as p
 import os
 import logging
-print("================================\n the project is begining\n")
-data_path = "D:/home/acgotaku/Downloads/new_data/"
+
+
+logging.basicConfig(filename = 'svm_test.log', level = logging.INFO)
+
+data_path = "../data/"
 train = pd.read_csv(data_path+'train_set.csv', nrows=2000)
 test = pd.read_csv(data_path+'test_set.csv',nrows=1)
+
 logging.info("================================\n the csv file is read \n")
 
 vect = TfidfVectorizer(ngram_range=(1,3), max_df=0.9, min_df= 3, smooth_idf=1, sublinear_tf=1)
@@ -20,14 +23,15 @@ train_label = np.asarray(train['class'], dtype=int)
 clf = SVC()
 
 param_grid = [
-    {"kernel":["linear","rbf","sigmoid"]},
-    {'C':np.logspace(-2, 1, 10)},
-    {'gamma':np.logspace(-3, 2, 10)}
+    {"kernel":["linear","rbf"]},
+    {'C':np.logspace(-2, 1, 2)},
+    {'gamma':np.logspace(-3, 2, 2)}
 ]
 cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
-grid = GridSearchCV(clf,param_grid=param_grid, cv=cv)
+grid = GridSearchCV(clf,param_grid=param_grid, cv=cv, n_jobs=-1)
 grid.fit(train_num, train_label)
 
+logging.info('================================\n the grid search is done! \n')
 #save the grid search result
 with open('./grid.result', 'wb') as f:
     p.dump(grid, f)
